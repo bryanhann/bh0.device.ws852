5bc58369b693818c8637993729d31d6032ac0161
Bryan Hann <nobody@nowhere>
2020-08-17 10:04:47 +0800

    Replace defunct [$BH0__RAW__]. Improve.

--- activate.sh
+++ activate.sh
@@ -1,6 +1,13 @@
-bh0-device-ws852.fetch () {
-    mkdir -p $BH0__RAW__/bh0.device.ws852
-    /Volumes/WS-852/device.ws852/process.sh
-    cp /Volumes/WS-852/device.ws852/__RAW__/* $BH0__RAW__/bh0.device.ws852
+bh0-device-ws852.acc () {
+    DEV=/Volumes/WS-852/device.ws852
+    ${DEV}/process.sh
+    echo ---copying from device to acc---
+    for file in ${DEV}/__RAW__/*.mp3 ; do
+        echo moving $file...
+        mv $file ${__BH0_ACC__}/.
+    done
+   # cp ${DEV}/__RAW__/* ${__BH0_ACC__}/.
+   # /Volumes/WS-852/device.ws852/process.sh
+   # cp /Volumes/WS-852/device.ws852/__RAW__/* ${__BH0_RAW__}/bh0.device.ws852
 }
 
